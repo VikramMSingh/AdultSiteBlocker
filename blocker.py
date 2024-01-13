0975910bb167d34f32a6bf638d487a51df7b5c90
Vikram Singh <vikramsingh@Vikrams-MacBook-Air.local>
2024-01-12 22:13:12 -0500

    Adding check for empty file

--- blocker.py
+++ blocker.py
@@ -1,7 +1,37 @@
-site_file = open('sites_to_block','r')
-list = site_file.read().splitlines()
-host_file = open('/etc/hosts','a')
-for i in list:
-	host_file.write(f'127.0.0.1	{i}\n')
-	host_file.write(f'::1	{i}\n')
-	print(f'Blocked {i}')
+import os
+#site_file = open('sites_to_block','r')
+#list = site_file.read().splitlines()
+#host_file = open('/etc/hosts','a')
+
+def checkFile(input_file):
+	if os.stat(input_file).st_size==0:
+		sys.exit('Empty input file')
+	else:
+		site_file = open(f'{input_file}','r')
+		site_list = site_file.read().splitlines()
+	return site_list
+
+def uniqueList(site_list):
+	st = set(site_list)
+	un = list(st)
+	return un
+
+def blockSite(l,hot_file):
+	x = 0
+	host_file = open(f'{hot_file}','a') 
+	for i in l:
+		host_file.write(f'127.0.0.1	{i}\n')
+		host_file.write(f'::1	{i}\n')
+		x += 1
+	print(f'Blocked {x} sites')
+
+def main():
+	site_file = 'sites_to_block'
+	host_file = '/etc/hosts'
+	a = checkFile(site_file)
+	b = uniqueList(a)
+	c = blockSite(b,host_file)
+	print(c)
+
+if __name__ == "__main__":
+	main()
